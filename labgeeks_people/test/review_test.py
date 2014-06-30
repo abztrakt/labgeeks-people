85d6e732d4f4bf1b5cac830360c31dcdd84cebfe
Cole Bessee <cbessee@uw.edu>
2014-06-30 13:54:52 -0700

    Adds badge photo, or if not available, profile photo to people list page

--- labgeeks_people/test/review_test.py
+++ labgeeks_people/test/review_test.py
@@ -6,6 +6,7 @@ from django.test import TestCase
 from django.test.client import Client
 from django.contrib.auth.models import User, Permission
 from django.contrib.contenttypes.models import ContentType
+from forms_builder.forms.models import *
 from labgeeks_people.models import *
 import datetime
 
@@ -21,7 +22,7 @@ class ReviewTestCase(TestCase):
         self.manager = User.objects.create_user('Manager', 'dawgm@test.com', 'pass')
         ct = ContentType.objects.get_for_model(UWLTReview)
         addReview = Permission.objects.get(content_type=ct, codename='add_uwltreview')
-        finalize = Permission.objects.get(content_type=ct, codename='finalize_uwltreview')
+        finalize = Permission.objects.get(content_type=ct, codename='finalize_review')
         self.manager.user_permissions.add(addReview)
         self.manager.save()
         self.bigboss = User.objects.create_user('BigBoss', 'dawgb@test.com', 'pass')
@@ -29,9 +30,9 @@ class ReviewTestCase(TestCase):
         self.bigboss.user_permissions.add(finalize)
         self.bigboss.save()
         d = datetime.date.today()
-        self.dawgreview = UWLTReview.objects.create(id=1, user=self.dawg, date=d, reviewer=self.manager, dependability=3, productivity=4, customer_service=3, teamwork=4, integrity=5, technical_knowledge=3, attitude=5, responsibility=2, policies=4, communication=2, initiative=4, procedures=3)
-        self.managerreview = UWLTReview.objects.create(id=2, user=self.manager, date=d, reviewer=self.bigboss, dependability=5, productivity=4, customer_service=5, teamwork=4, integrity=5, technical_knowledge=4, attitude=5, responsibility=4, policies=4, communication=4, initiative=4, procedures=3)
-        self.bigbossreview = UWLTReview.objects.create(id=3, user=self.bigboss, date=d, reviewer=self.manager, dependability=5, productivity=4, customer_service=4, teamwork=4, integrity=5, technical_knowledge=5, attitude=5, responsibility=5, policies=4, communication=2, initiative=4, procedures=4)
+        self.reviewform = ReviewForm.objects.create()
+        self.dawgreview = ReviewFormEntry.objects.create(reviewing=self.dawg, form=self.reviewform, official=True, reviewer=self.bigboss, complete=True)
+        self.managerreview = ReviewFormEntry.objects.create(reviewing=self.manager, form=self.reviewform, official=True, reviewer=self.bigboss, complete=True)
         self.dawgprofile = UserProfile.objects.create(user=self.dawg)
         self.managerprofile = UserProfile.objects.create(user=self.manager)
         self.bigbossprofile = UserProfile.objects.create(user=self.bigboss)
@@ -42,17 +43,13 @@ class ReviewTestCase(TestCase):
         """
         c = Client()
         c.login(username="Dawg", password='pass')
-        resp3 = c.get('/people/Dawg/view_reviews/info/?id=1')
-        self.assertEqual(resp3.status_code, 200)  # access to his own json review I guess
-        resp4 = c.get('/people/Manager/view_reviews/info/?id=2')
-        self.assertContains(resp4, "You do not have permission")  # no access to others jsons
         resp1 = c.get('/people/Dawg/view_reviews/')
-        self.assertEqual(resp1.status_code, 200)
-        self.assertContains(resp1, "Reviews for Dawg")
-        resp = c.get('/people/Dawg/review/')
-        self.assertEqual(resp.status_code, 403)
-        resp2 = c.get('/people/Manager/view_reviews/')
-        self.assertEqual(resp2.status_code, 403)
+        self.assertEqual(resp1.status_code, 200)  # access to his own json review I guess
+        resp2 = c.get('/people/Dawg/review/')
+        self.assertEqual(resp2.status_code, 200)
+        self.assertContains(resp2, "Reviews for Dawg")
+        resp3 = c.post('/people/Dawg/review/', {'reviewing': 'Dawg', 'reviewer': 'Dawg', 'complete': True, 'awesomeness': 5, 'comments': 'Dawg is a cool dawg'}, follow=True)
+        self.assertEqual(resp3.status_code, 200)
         c.logout()
 
     def testManagerPermissions(self):
@@ -66,16 +63,12 @@ class ReviewTestCase(TestCase):
         self.assertEqual(resp1.status_code, 200)
         self.assertContains(resp1, 'Reviews for Manager')
         resp2 = c.get('/people/Dawg/view_reviews/')
-        self.assertEqual(resp2.status_code, 403)
+        self.assertEqual(resp2.status_code, 200)
+        self.assertContains(resp2, "Official Reviews for Dawg")
         resp = c.get('/people/Dawg/review/')
         self.assertEqual(resp.status_code, 200)
         resp3 = c.get('/people/BigBoss/view_reviews/')
-        self.assertEqual(resp3.status_code, 403)
-        resp4 = c.get('/people/Manager/view_reviews/info/?id=2')
-        self.assertEqual(resp4.status_code, 200)
-        resp5 = c.get('/people/Dawg/view_reviews/info/?id=1')
-        self.assertEqual(resp5.status_code, 200)
-        self.assertContains(resp5, 'You do not have permission')
+        self.assertEqual(resp3.status_code, 200)
         c.logout()
 
     def testBigBossPermissions(self):
@@ -90,10 +83,12 @@ class ReviewTestCase(TestCase):
         self.assertEqual(resp.status_code, 200)
         resp2 = c.get('/people/BigBoss/review/')
         self.assertEqual(resp2.status_code, 200)
-        resp3 = c.get('/people/Dawg/view_reviews/info/?id=1')
-        self.assertEqual(resp3.status_code, 200)
-        resp4 = c.get('/people/BigBoss/view_reviews/info/?id=3')
-        self.assertEqual(resp4.status_code, 200)
-        resp5 = c.get('/people/Manager/view_reviews/info/?id=4')
-        self.assertEqual(resp5.status_code, 403)
         c.logout()
+
+    def testReviewForm(self):
+        pass
+
+    def testReviewFormEtnry(self):
+        pass
+
+    def 
--- labgeeks_people/tests.py
+++ labgeeks_people/tests.py
@@ -4,3 +4,5 @@ from labgeeks_people.test.login_test import LoginTestCase
 from labgeeks_people.test.profile_test import ProfileTestCase
 from labgeeks_people.test.wage_history_test import WageHistoryTestCase
 from labgeeks_people.test.review_test import ReviewTestCase
+
+#Write Unit Tests for New Reviews Here
